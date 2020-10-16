4998d6e9a7a145aa6e4827e4d1c54868c7859ec6
Daniel Esteves <estevesd8@gmail.com>
2020-10-15 21:20:50 -0400

    :bug: Changing url to generate the images

--- functions/process-url/process-url.js
+++ functions/process-url/process-url.js
@@ -1,15 +1,15 @@
-const qs = require('querystring')
-const cloudinary = require('cloudinary').v2
+const qs = require("querystring");
+const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD,
   api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
-})
+});
 
 // eslint-disable-next-line
 exports.handler = async function (event, ctx) {
-  const { queryStringParameters: query } = event
+  const { queryStringParameters: query } = event;
 
   try {
     const imageUrl = cloudinary.url(
@@ -19,22 +19,22 @@ exports.handler = async function (event, ctx) {
         sign_url: true,
         secure: true,
         custom_pre_function: {
-          function_type: 'remote',
-          source: `https://opengraphimg.com/.netlify/functions/generate-opengraph?${qs.stringify(
+          function_type: "remote",
+          source: `https://generator.opengraphimg.com/view?${qs.stringify(
             query
           )}`,
         },
       }
-    )
+    );
 
     return {
       statusCode: 302,
       headers: {
         Location: imageUrl,
       },
-      body: '',
-    }
+      body: "",
+    };
   } catch (e) {
-    throw new Error(e)
+    throw new Error(e);
   }
-}
+};
