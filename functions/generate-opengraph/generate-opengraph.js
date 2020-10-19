4e108f5c0ef5e733622fa74d69cebd0e050be1c1
Daniel Esteves <estevesd8@gmail.com>
2020-10-18 21:43:22 -0400

    :bug: Using ternary operator to verify author

--- functions/generate-opengraph/generate-opengraph.js
+++ functions/generate-opengraph/generate-opengraph.js
@@ -1,29 +1,29 @@
 // Dependencies
-const path = require('path')
-const fs = require('fs')
-const chromium = require('chrome-aws-lambda')
+const path = require("path");
+const fs = require("fs");
+const chromium = require("chrome-aws-lambda");
 
 // Script
-const filePath = path.join(__dirname, 'image.js')
-const script = fs.readFileSync(filePath, 'utf-8')
+const filePath = path.join(__dirname, "image.js");
+const script = fs.readFileSync(filePath, "utf-8");
 
 exports.handler = async (event, context) => {
   await chromium.font(
-    'https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf'
-  )
+    "https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf"
+  );
 
   const browser = await chromium.puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath,
     headless: chromium.headless,
-  })
-  const page = await browser.newPage()
+  });
+  const page = await browser.newPage();
 
   page.setViewport({
     width: 1200,
     height: 630,
-  })
+  });
 
   await page.setContent(`
     <!DOCTYPE html>
@@ -44,7 +44,7 @@ exports.handler = async (event, context) => {
         </div>
       </body>
     </html>
-  `)
+  `);
 
   const {
     queryStringParameters: {
@@ -67,60 +67,60 @@ exports.handler = async (event, context) => {
       authorColor,
       authorSize,
     },
-  } = event
-  const tags = tagsParam ? decodeURIComponent(tagsParam).split(',') : []
+  } = event;
+  const tags = tagsParam ? decodeURIComponent(tagsParam).split(",") : [];
   await page.addScriptTag({
     content: `
-      window.title = "${title || 'No Title'}";
+      window.title = "${title || "No Title"}";
       window.tags = ${JSON.stringify(tags)};
-      window.author = "${decodeURI(author) || ''}";
-      window.background = "${background || 'fff'}";
+      window.author = "${author ? decodeURI(author) : ""}";
+      window.background = "${background || "fff"}";
 
       // Box Styles
-      window.boxBackground = "${boxBackground || '1a202c'}";
+      window.boxBackground = "${boxBackground || "1a202c"}";
 
       // Title Styles
-      window.titleColor = "${titleColor || 'fff'}";
-      window.titleAlign = "${titleAlign || 'text-center'}";
-      window.titleSize = "${titleSize || 'text-6xl'}";
-      window.titleMargin = "${titleMargin || 'm-0'}";
+      window.titleColor = "${titleColor || "fff"}";
+      window.titleAlign = "${titleAlign || "text-center"}";
+      window.titleSize = "${titleSize || "text-6xl"}";
+      window.titleMargin = "${titleMargin || "m-0"}";
 
       // Details Styles
-      window.detailsMargin = "${detailsMargin || '-m-20'}";
+      window.detailsMargin = "${detailsMargin || "-m-20"}";
 
       // Tags Styles
-      window.tagsColor = "${tagsColor || 'fff'}";
-      window.tagsSize = "${tagsSize || 'text-2xl'}";
+      window.tagsColor = "${tagsColor || "fff"}";
+      window.tagsSize = "${tagsSize || "text-2xl"}";
 
       // Author Styles
-      window.logoUrl = "${logoUrl || ''}";
-      window.logoSize = "${logoSize || 'w-10 h-10'}";
-      window.logoMargin = "${logoMargin || 'mr-4'}";
+      window.logoUrl = "${logoUrl || ""}";
+      window.logoSize = "${logoSize || "w-10 h-10"}";
+      window.logoMargin = "${logoMargin || "mr-4"}";
       window.atSymbol = ${atSymbol || false};
-      window.authorColor = "${authorColor || 'fff'}";
-      window.authorSize = "${authorSize || 'text-4xl'}";
+      window.authorColor = "${authorColor || "fff"}";
+      window.authorSize = "${authorSize || "text-4xl"}";
     `,
-  })
-  await page.addScriptTag({ content: script })
+  });
+  await page.addScriptTag({ content: script });
 
   const boundingReact = await page.evaluate(() => {
-    const app = document.getElementById('app')
-    const { x, y, width, height } = app.children[0].getBoundingClientRect()
+    const app = document.getElementById("app");
+    const { x, y, width, height } = app.children[0].getBoundingClientRect();
 
-    return { x, y, width, height }
-  })
+    return { x, y, width, height };
+  });
 
-  const screenshotBuffer = await page.screenshot({ clip: boundingReact })
+  const screenshotBuffer = await page.screenshot({ clip: boundingReact });
 
-  await browser.close()
+  await browser.close();
 
   return {
     isBase64Encoded: true,
     statusCode: 200,
     headers: {
-      'Content-Type': 'image/png',
-      'Content-Length': screenshotBuffer.length.toString(),
+      "Content-Type": "image/png",
+      "Content-Length": screenshotBuffer.length.toString(),
     },
-    body: screenshotBuffer.toString('base64'),
-  }
-}
+    body: screenshotBuffer.toString("base64"),
+  };
+};
